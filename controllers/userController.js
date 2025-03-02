69055631ac80d91b3a37a3d9d3ff21c58cfa41c0
Awais <awais.ali1312@gmail.com>
2025-03-02 22:56:13 +0200

    Password Hashing and Auth

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,10 +1,25 @@
 import User from "../models/Users.js";
+import { hashedPassword, comparePassword } from "../utils/passwordUtils.js";
+import { createJWT } from "../utils/tokenUtils.js";
 
 export const register = async (req, res) => {
   const isAccountFirst = (await User.countDocuments()) === 0;
+  const hash = await hashedPassword(req.body.password);
+  req.body.password = hash;
   req.body.role = isAccountFirst ? "Admin" : "User";
   const user = await User.create(req.body);
   res.status(201).json({ user });
 };
 
-export const login = async (req, res) => {};
+export const login = async (req, res) => {
+  const loginUser = await User.findOne({ email: req.body.email });
+  if (!loginUser) {
+    return res.status(404).json({ message: "Email or Password is incorrect." });
+  }
+  const matchUser = await comparePassword(req.body.password, loginUser.password);
+  if (!matchUser) {
+    return res.status(404).json({ message: "Email or Password is incorrect." });
+  }
+  const token = createJWT({ userId: loginUser._id, role: loginUser.role });
+  return res.json({ token });
+};
